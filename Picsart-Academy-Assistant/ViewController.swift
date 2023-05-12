e0d2daa82c014191f0680e6bc18918b48018c715
Aren <aren.musayelyan555@gmail.com>
2023-05-12 19:56:12 +0400

    Initial Commit

---
+++ Picsart-Academy-Assistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Picsart-Academy-Assistant
+//
+//  Created by Aren Musayelyan on 12.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
