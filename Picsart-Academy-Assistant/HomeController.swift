48215bb5e36c096c5a38229cc985932be17524ce
Aren <aren.musayelyan555@gmail.com>
2023-05-12 20:03:55 +0400

    Getting Started

---
+++ Picsart-Academy-Assistant/HomeController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeController.swift
+//  Picsart-Academy-Assistant
+//
+//  Created by Aren Musayelyan on 12.05.23.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
